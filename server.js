b78ed7b73ed8435d3cb7d11a7b554a6a811ca72a
Aditya Pandey <adityapandey4940@gmail.com>
2026-05-25 12:30:57 +0530

    configure cors

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5173",
             "http://192.168.5.129:5173",
-            "http://192.168.5.212:5173"],
+            "http://192.168.5.212:5173",
+        "https://event-management-frontend-yuww.vercel.app/"],
         methods: ['GET', 'POST'],
         credentials: true
     }
